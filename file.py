e62c9b68377c1c93471f8b99b5e66f331ed54771
tonderys <tonszym@gmail.com>
2013-10-12 17:03:32 +0200

    added 2 unit tests for File_Changer class

--- file.py
+++
@@ -1,9 +0,0 @@
-from File_Finder import *
-from File_Changer import *
-
-finder = File_Finder(sys.argv[2], sys.argv[1])
-
-#for path in finder.file_paths:
-#    change_file(path)
-
-finder.print_file_paths()
