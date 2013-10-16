d28b78a3e426111e865331a26309e4a856aebed8
Szymon <szymon.tonderys@nsn.com>
2013-10-16 09:31:09 +0200

    file changer tests refactored

--- File_Changer_test.py
+++ File_Changer_test.py
@@ -6,62 +6,48 @@ from File_Changer import *
 
 filename = "testTmpFile"
 
-class TestInit(unittest.TestCase):
+class UnitTests(unittest.TestCase):
     def setUp(self):
         if os.path.exists(filename):
             os.remove(filename)
+    
     def tearDown(self):
         if os.path.exists(filename):
             os.remove(filename)
         if os.path.exists(filename+"~"):
             os.remove(filename+"~")
 
-class no_input_file(TestInit):
-    def runTest(self):
-        self.failUnlessEqual(File_Changer(filename, "[0-4]", 4, "4", "2").output, "brak pliku/uprawnien")
-        self.failIf(os.path.exists(filename+"~"))
+    def test_no_input_file(self):
+        self.assertEqual(File_Changer(filename, "[0-4]", 4, "4", "2").output, "brak pliku/uprawnien")
+        self.assertFalse(os.path.exists(filename+"~"))
 
-class pattern_not_found(TestInit):
-    def runTest(self):
+    def test_pattern_not_found(self):
         output = open(filename, "w")
         output.write("0000")
         output.close()
         
-        self.failUnlessEqual(File_Changer(filename, "[0-4]", 4, "4", "2").output, "w pliku "+filename+" nie znaleziono szukanej frazy")
-        self.failIf(os.path.exists(filename+"~"))
+        self.assertEqual(File_Changer(filename, "[0-4]", 4, "4", "2").output, "w pliku "+filename+" nie znaleziono szukanej frazy")
+        self.assertFalse(os.path.exists(filename+"~"))
 
-class single_line_with_pattern(TestInit):
-    def runTest(self):
+    def test_single_line_with_pattern(self):
         output = open(filename, "w")
         output.write("4444")
         output.close()
         
-        self.failUnlessEqual(File_Changer(filename, "[0-4]", 4, "4", "2").output, "zmiana pliku "+filename)
-        self.failUnless(os.path.exists(filename+"~"))
+        self.assertEqual(File_Changer(filename, "[0-4]", 4, "4", "2").output, "zmiana pliku "+filename)
+        self.assertTrue(os.path.exists(filename+"~"))
 
-class correctly_executed_program(TestInit):
-    def runTest(self):
+    def test_correctly_executed_program(self):
         output = open(filename, "w")
         output.write("0000\n01234\na324\n0004\n4444\n")
         output.close()
  
-        self.failUnlessEqual(File_Changer(filename, "[0-4]", 4, "4", "2").output, "zmiana pliku "+filename)
+        self.assertEqual(File_Changer(filename, "[0-4]", 4, "4", "2").output, "zmiana pliku "+filename)
 
         output = open(filename, "r")
-        self.failUnlessEqual (output.read(), "0000\n01234\na324\n0002\n2222\n")
+        self.assertEqual (output.read(), "0000\n01234\na324\n0002\n2222\n")
         output.close()
 
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(correctly_executed_program())
-    suite.addTest(no_input_file())
-    suite.addTest(pattern_not_found())
-    suite.addTest(single_line_with_pattern())
-    return suite
-
 if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    test_suite = suite()
-    runner.run (test_suite)
+    unittest.main()
 
