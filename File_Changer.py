3c98f168ad4d56cd687fdbf3ce5cf1cd935f675a
Szymon <szymon.tonderys@nsn.com>
2013-10-22 13:22:41 +0200

    GUI and program logic fixed and finished

--- File_Changer.py
+++ File_Changer.py
@@ -5,13 +5,14 @@ import re
 
 class File_Changer:
     output_codes_dictionary={1:"zmiana pliku %s", 2:"w pliku %s nie znaleziono szukanej frazy", 3:"brak pliku/uprawnien do pliku: %s"}
-    def __init__(self, filename, searched_phrase="[0-4]", line_length=4 , old_phrase="4", new_phrase="2"):
+    def __init__(self, filename, searched_phrase, line_length , old_phrase, new_phrase, quiet = 1):
+        self.quiet = quiet
         self.output_code = 0
-        self.filename = filename
+        self.filename = str(filename)
         self.line_length = line_length
         self.searched_phrase = "%s{%s}" % (searched_phrase, line_length)
-        self.old_phrase = old_phrase
-        self.new_phrase = new_phrase
+        self.old_phrase = str(old_phrase)
+        self.new_phrase = str(new_phrase)
         self.change_file()
 
     def file_match(self):
@@ -32,7 +33,7 @@ class File_Changer:
         except IOError:
             self.output_code = 3
     
-    def change_file(self, quiet = 1):
+    def change_file(self):
         filename = self.filename
         searched_phrase = self.searched_phrase
         old_phrase = self.old_phrase
@@ -52,7 +53,7 @@ class File_Changer:
                 source.close()
         except (IOError):
             self.output_code = 3
-        if quiet == 0:
+        if self.quiet == 0:
             print self.output_codes_dictionary[self.output_code] % self.filename        
 
 if __name__ == "__main__":
--- File_Changer_GUI.py
+++ File_Changer_GUI.py
@@ -1,36 +1,81 @@
 from Tkinter import *
 from threading import Thread
 from File_Finder import *
+from File_Changer import *
+from colour import Color
 
 class File_Changer_GUI:
-    path = ""
-    extension = ""
 
     def __init__(self):
         self.top_window = Tk()
         self.path_label = Label(self.top_window, text = "path")
-        self.path_label.grid(column = 1, row = 1)
-        self.path_entry = Entry(self.top_window, textvariable = self.path, width = 30)
-        self.path_entry.grid(column = 2, row = 1)
+        self.path_label.grid(column = 1, row = 2)
+        self.path_entry = Entry(self.top_window, width = 30)
+        self.path_entry.insert(0,"./")
+        self.path_entry.grid(column = 2, row = 2, columnspan = 2)
         self.extension_label = Label(self.top_window, text = "extension")
-        self.extension_label.grid(column = 1, row = 2)
-        self.extension_entry = Entry(self.top_window, textvariable = self.extension, width = 30)
-        self.extension_entry.grid(column = 2, row = 2)
-        self.scroll = Scrollbar(self.top_window)
-        self.scroll.grid(column = 3, row = 1, rowspan = 3)
-        self.files_list = Listbox(self.top_window)
-        self.files_list.pack(self.scroll)
-        self.files_list.config(yscrollcommand = self.scroll.set)
-        self.scroll.config (command = self.files_list.yview )
+        self.extension_label.grid(column = 1, row = 3)
+        self.extension_entry = Entry(self.top_window, width = 30)
+        self.extension_entry.insert(0,"sm")
+        self.extension_entry.grid(column = 2, row = 3, columnspan = 2)
+        self.files_label = Label(self.top_window, text = "found files:")
+        self.files_label.grid(column = 4, row = 1, columnspan = 2)
+        self.files_list = Listbox(self.top_window, width = 60)
+        self.files_list.grid(column = 4, row = 2, rowspan = 3, columnspan = 2)
         self.search_button = Button(self.top_window, text = "Search", command = self.search_for_files_and_put_to_list)
-        self.search_button.grid (column = 1, columnspan = 2, row = 3)
+        self.search_button.grid (column = 1, columnspan = 3, row = 4)
+
+        self.searched_phrase_label = Label(self.top_window, text = "searched phrase")
+        self.searched_phrase_label.grid (column = 1, row = 6)
+        self.searched_phrase_entry = Entry(self.top_window, width = 30)
+        self.searched_phrase_entry.insert(0,"[0-4]")
+        self.searched_phrase_entry.grid(column = 2, columnspan = 2, row = 6)
+        self.old_phrase_label = Label(self.top_window, text = "old phrase")
+        self.old_phrase_label.grid(column = 1, row = 7)
+        self.old_phrase_entry = Entry(self.top_window, width = 30)
+        self.old_phrase_entry.insert(0, "4")
+        self.old_phrase_entry.grid(column = 2, columnspan = 2, row = 7)
+        self.new_phrase_label = Label(self.top_window, text = "new phrase")
+        self.new_phrase_label.grid(column = 1, row = 8)
+        self.new_phrase_entry = Entry(self.top_window, width = 30)
+        self.new_phrase_entry.insert(0, "2")
+        self.new_phrase_entry.grid(column = 2, columnspan = 2, row = 8)
+        self.line_length_label = Label(self.top_window, text = "line length")
+        self.line_length_label.grid(column = 1, row = 9)
+        self.line_length_entry = Entry(self.top_window, width = 3) 
+        self.line_length_entry.insert(0, "4")
+        self.line_length_entry.grid(column = 2, row =9, sticky = "w")
+        self.change_button = Button(self.top_window, text = "change", command = self.change_files)
+        self.change_button.grid(column = 3, row = 9)
+        self.changed_files_label = Label(self.top_window, text = "changed files:")
+        self.changed_files_label.grid(column = 4, row = 5)
+        self.changed_files_list = Listbox(self.top_window, width = 30, fg = Color("green"))
+        self.changed_files_list.grid(column = 4, row = 6, rowspan = 4)
+        self.unchanged_files_label = Label(self.top_window, text = "unchanged files:")
+        self.unchanged_files_label.grid(column = 5, row = 5)
+        self.unchanged_files_list = Listbox(self.top_window, width = 30, fg = Color("red"))
+        self.unchanged_files_list.grid(column = 5, row = 6, rowspan = 4)
 
         self.top_window.mainloop()
 
     def search_for_files_and_put_to_list(self):
+        i = 0
         self.files_list.delete(0, END)
         self.finder = File_Finder(self.path_entry.get(), self.extension_entry.get())
         for file_path in self.finder.file_paths:
-            self.files_list.insert(0,file_path)
+            self.files_list.insert(i,file_path)
+            i += 1
 
+    def change_files(self):
+        i = 0
+        self.changed_files_list.delete(0, END)
+        for file_path in self.files_list.get(0, END):
+            changer = File_Changer(str(file_path), str(self.searched_phrase_entry.get()), int(self.line_length_entry.get()), str(self.old_phrase_entry.get()), str(self.new_phrase_entry.get()), 0)
+            print "File_Changer(%s, %s, %s, %s, %s)" % (file_path, self.searched_phrase_entry.get(), self.line_length_entry.get(), self.old_phrase_entry.get(), self.new_phrase_entry.get())
+            if changer.output_code != 1:
+                self.unchanged_files_list.insert(i, file_path)
+                i += 1
+            else:
+                self.changed_files_list.insert(i, file_path)
+                i+= 1
 gui = File_Changer_GUI()
